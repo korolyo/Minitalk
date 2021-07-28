b73f389e04c86b82deb1f45fcc3074bc7f8b5622
korolyo <korolyo@gmail.com>
2021-07-28 18:01:30 +0300

    mandatory is perfect

--- minitalk/ft_server.c
+++ minitalk/ft_server.c
@@ -1,45 +1,37 @@
 #include "ft_minitalk.h"
-#include <stdio.h>
 
-void	handler_s1(int num)
+void	handler_s1(int num, siginfo_t *siginfo, void *context)
 {
-//    int		i;
-    char	c;
+    static int	count;
+    static char	c;
 
-    while (i >= 0)
+	if (!count)
 	{
-		if (num == SIGUSR1)
-		{
-			str[i] = '0';
-			printf("%s\n", str);
-			i--;
-		}
+		c = 0;
+		count = 1;
+	}
+    if (count != 256)
+	{
 		if (num == SIGUSR2)
-		{
-			str[i] = '1';
-			printf("%s\n", str);
-			i--;
-		}
-		if (i == 0)
-		{
-			c = ft_atoi(str);
-			ft_putchar_fd(c, 1);
-			i--;
-		}
+			c = c | count;
+		count *= 2;
+	}
+	if (count == 256)
+	{
+		ft_putchar_fd(c, 1);
+		c = 0;
+		count = 1;
 	}
 }
 
 int main(void)
 {
 	struct sigaction	sa;
-//	char    *str;
-//	int     i;
+	int 				cpid;
 
-//	i = 7;
-//	str = (char *)malloc(sizeof (char) * 9);
-	str[8] = '\0';
-	sa.sa_handler = handler_s1;
-	sa.sa_flags = 0;
+	sa.sa_sigaction = handler_s1;
+	sa.sa_flags = SA_SIGINFO;
+	cpid = si_pid.siginfo_t;
 	sigemptyset(&sa.sa_mask);
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
--- minitalk/includes/ft_minitalk.h
+++ minitalk/includes/ft_minitalk.h
@@ -5,7 +5,5 @@
 # include <sys/types.h>
 # include <unistd.h>
 # include "libft.h"
-int		i = 7;
-char	str[9];
 
 #endif
